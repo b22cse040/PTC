92945b57bc88507a406dcf336185b647cbcb9141
b22cse040 <b22cs040@iitj.ac.in>
2026-08-28 18:56:41 +0530

    minor refactoring

--- src/tools/tool_executors.py
+++ src/tools/tool_executors.py
@@ -3,43 +3,14 @@ from src.tools.native_tools import *
 # from mcp_tools import *
 
 TOOL_EXECUTORS: Dict[str, ToolExecutor] = {
-    "get_employees": NativeToolExecutor(
-        get_employees,
-    ),
-
-    "get_employees_by_department": NativeToolExecutor(
-        get_employees_by_department,
-    ),
-
-    "get_employee_department": NativeToolExecutor(
-        get_employee_department,
-    ),
-
-    "get_employee_salary": NativeToolExecutor(
-        get_employee_salary,
-    ),
-
-    "get_employee_benefits": MCPToolExecutor(
-        "get_employee_benefits",
-    ),
-
-    "calculate_employee_bonus": MCPToolExecutor(
-        "calculate_employee_bonus",
-    ),
-
-    "get_company_holidays": MCPToolExecutor(
-        "get_company_holidays",
-    ),
-
-    "get_employee_attendance": MCPToolExecutor(
-        "get_employee_attendance",
-    ),
-
-    "get_employee_projects": MCPToolExecutor(
-        "get_employee_projects",
-    ),
-
-    "get_project_status": MCPToolExecutor(
-        "get_project_status",
-    ),
+    "get_employees": NativeToolExecutor(get_employees),
+    "get_employees_by_department": NativeToolExecutor(get_employees_by_department),
+    "get_employee_department": NativeToolExecutor(get_employee_department),
+    "get_employee_salary": NativeToolExecutor(get_employee_salary),
+    "get_employee_benefits": MCPToolExecutor("get_employee_benefits"),
+    "calculate_employee_bonus": MCPToolExecutor("calculate_employee_bonus"),
+    "get_company_holidays": MCPToolExecutor("get_company_holidays"),
+    "get_employee_attendance": MCPToolExecutor("get_employee_attendance"),
+    "get_employee_projects": MCPToolExecutor("get_employee_projects"),
+    "get_project_status": MCPToolExecutor("get_project_status"),
 }
