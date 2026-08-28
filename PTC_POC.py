34d0ee16fec123b30f90cbb80af18f84e0bb8d7a
b22cse040 <b22cs040@iitj.ac.in>
2026-08-28 18:53:12 +0530

    Rename PTC packages

--- PTC_POC.py
+++ PTC_POC.py
@@ -10,7 +10,7 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 # from src.ptc.ptc_anthropic.ptcAnthropic import PTCAnthropic
-from src.ptc.ptc_openai.PTCOpenAI import PTCOpenAI
+from src.ptc.ptc_openai.ptcOpenAI import PTCOpenAI
 
 from src.tool_search_module.tool_search import ToolSearchModule
 from src.tools.tool_definitions import TOOL_DEFINITIONS
